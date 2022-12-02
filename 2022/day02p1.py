5403b270eff74dca0ca6cde74f9559fc4be3c579
hyper-neutrino <alex.yj.liao@gmail.com>
2022-12-02 00:28:06 -0500

    Create day02p1.py

---
+++ 2022/day02p1.py
@@ -0,0 +1,16 @@
+t = 0
+
+for line in open(0).read().splitlines():
+    x, y = line.split()
+
+    x = ord(x) - 65
+    y = ord(y) - ord("Y")
+
+    if x == y:
+        t += 3
+    elif (y - x) % 3 == 1:
+        t += 6
+    
+    t += y + 1
+
+print(t)

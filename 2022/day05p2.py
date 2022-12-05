21aefb8ee0ba80bcca6d8a78934f455dca10bb64
hyper-neutrino <alex.yj.liao@gmail.com>
2022-12-05 01:23:26 -0500

    Update day05p2.py

--- 2022/day05p2.py
+++ 2022/day05p2.py
@@ -6,7 +6,7 @@ x = open(0)
 
 for line in x:
     if line == "\n": break
-    s.append([line[k * 4 + 1] for k in range(len(line) // 4)])
+    s.append([line[k * 4 + 1] for k in range(len(line) // 4)]) # this is just line[1::4] - thanks UnrelatedString
 
 s.pop()
 s = [list("".join(c).strip()[::-1]) for c in zip(*s)]

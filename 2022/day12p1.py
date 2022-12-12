6205b669ba095694c9a86b815700b8f96ab64ee7
hyper-neutrino <alex.yj.liao@gmail.com>
2022-12-12 00:49:51 -0500

    Create day12p1.py

---
+++ 2022/day12p1.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+grid = [list(x) for x in open(0).read().strip().splitlines()]
+
+for r, row in enumerate(grid):
+    for c, item in enumerate(row):
+        if item == "S":
+            sr = r
+            sc = c
+            grid[r][c] = "a"
+        if item == "E":
+            er = r
+            ec = c
+            grid[r][c] = "z"
+
+q = deque()
+q.append((0, sr, sc))
+
+vis = {(sr, sc)}
+
+while q:
+    d, r, c = q.popleft()
+    for nr, nc in [(r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)]:
+        if nr < 0 or nc < 0 or nr >= len(grid) or nc >= len(grid[0]):
+            continue
+        if (nr, nc) in vis:
+            continue
+        if ord(grid[nr][nc]) - ord(grid[r][c]) > 1:
+            continue
+        if nr == er and nc == ec:
+            print(d + 1)
+            exit(0)
+        vis.add((nr, nc))
+        q.append((d + 1, nr, nc))

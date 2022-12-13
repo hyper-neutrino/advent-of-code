ee521f58bf839b4ec7bdeadb31cee4114b350f7b
hyper-neutrino <alex.yj.liao@gmail.com>
2022-12-13 00:30:13 -0500

    Create day13p2.py

---
+++ 2022/day13p2.py
@@ -0,0 +1,30 @@
+def f(x, y):
+    if type(x) == int:
+        if type(y) == int:
+            return x - y
+        else:
+            return f([x], y)
+    else:
+        if type(y) == int:
+            return f(x, [y])
+    
+    for a, b in zip(x, y):
+        v = f(a, b)
+        if v:
+            return v
+    
+    return len(x) - len(y)
+
+x = list(map(eval, open(0).read().split())) + [[[2]], [[6]]]
+
+i2 = 1
+i6 = 2
+
+for a in x:
+    if f(a, [[2]]) < 0:
+        i2 += 1
+        i6 += 1
+    elif f(a, [[6]]) < 0:
+        i6 += 1
+
+print(i2 * i6)

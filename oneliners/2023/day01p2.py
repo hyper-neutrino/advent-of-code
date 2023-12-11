c9d9a4fa7a2b7cf6092fc092c6cad66059db8a74
hyper-neutrino <alex.yj.liao@gmail.com>
2023-12-11 15:00:46 -0500

    Add 2023/1/2 oneliner

---
+++ oneliners/2023/day01p2.py
@@ -0,0 +1,1 @@
+print(sum(int(digits[0] + digits[-1]) for digits in [[char for char in line.replace("one", "o1e").replace("two", "t2o").replace("three", "t3e").replace("four", "f4r").replace("five", "f5e").replace("six", "s6x").replace("seven", "s7n").replace("eight", "e8t").replace("nine", "n9e") if char.isdigit()] for line in open(0)]))

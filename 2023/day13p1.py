60067bbd6e94fe61597f5a0397a7e10e40488c88
hyper-neutrino <alex.yj.liao@gmail.com>
2023-12-13 01:43:55 -0500

    upload 2023/13

---
+++ 2023/day13p1.py
@@ -0,0 +1,25 @@
+def find_mirror(grid):
+    for r in range(1, len(grid)):
+        above = grid[:r][::-1]
+        below = grid[r:]
+        
+        above = above[:len(below)]
+        below = below[:len(above)]
+        
+        if above == below:
+            return r
+        
+    return 0
+
+total = 0
+
+for block in open(0).read().split("\n\n"):
+    grid = block.splitlines()
+
+    row = find_mirror(grid)
+    total += row * 100
+
+    col = find_mirror(list(zip(*grid)))
+    total += col
+
+print(total)
